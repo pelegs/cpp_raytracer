1e5cda49e4c1e80293117b0c0f5ec3602c673361
pelegs <pelegs@gmail.com>
2023-06-08 13:11:37 +0200

    Need to correct Plane init

--- classes.cpp
+++ classes.cpp
@@ -12,7 +12,9 @@
 typedef glm::vec<2, double> vec2_d;
 typedef glm::vec<3, double> vec3_d;
 typedef glm::vec<4, double> vec4_d;
+typedef glm::mat<2, 2, double> mat22_d;
 typedef glm::mat<3, 3, double> mat33_d;
+typedef glm::mat<4, 4, double> mat44_d;
 
 
 // General constants
@@ -37,7 +39,9 @@ const vec3_d Z_ = {.0, .0, 1.0};
 const vec2_d O2_ = {.0, .0};
 const vec3_d O3_ = {.0, .0, .0};
 const vec4_d O4_ = {.0, .0, .0, .0};
-const mat33_d I3 = {X_, Y_, Z_};
+const mat22_d I2 = glm::mat2(1.0);
+const mat33_d I3 = glm::mat3(1.0);
+const mat44_d I4 = glm::mat4(1.0);
 
 
 // ------------------- //
@@ -135,50 +139,55 @@ class Line
 
 class Plane
 {
-    vec3_d p0;
+    vec3_d n, v1, v2;
     vec4_d normal_form;
-    vec3_d n;
+    mat33_d pts;
 
   public:
     Plane()
     {
-        normal_form = glm::normalize(vec4_d(0.0, 1.0, 0.0, 1.0));
+        normal_form = vec4_d(0.0, 1.0, 0.0, -1.0);
         n = normal_form;
+        vec3_d p0 = {1.0, 1.0, 0.0};
+        vec3_d p1 = {0.0, 1.0, 1.0};
+        vec3_d p2 = {0.0, 1.0, 0.0};
+        pts = {p0, p1, p2};
     }
 
-    Plane(const mat33_d &pnts)
+    // TODO: correct Plane init!!
+
+    Plane(const mat33_d &pts)
     {
         // init from three points
-        p0 = pnts[0];
-        vec3_d v1 = glm::normalize(pnts[1] - pnts[0]);
-        vec3_d v2 = glm::normalize(pnts[2] - pnts[0]);
-        vec3_d v3 = glm::cross(v1, v2);
-        double d = -1.0*glm::dot(v3, pnts[0]);
-        this->normal_form = vec4_d(v3, d);
-        this->n = vec3_d(this->normal_form);
+        this->pts = pts;
+        v1 = pts[1] - pts[0];
+        v2 = pts[2] - pts[0];
+        n = glm::normalize(glm::cross(v1, v2));
+        double d = -1.0*glm::dot(n, pts[0]);
+        normal_form = vec4_d(n, d);
         // TODO: check that this is correct!
     }
 
-    Plane(const vec4_d &normal_form)
-    {
-        // init from normal form only
-        vec3_d norm = normal_form;
-        double nflen = glm::length(norm);
-        this->n = glm::normalize(norm);
-        double d = normal_form[3]/nflen;
-        this->normal_form = vec4_d(this->n, d);
-        int i = first_non_zero_index(normal_form);
-        this->p0 = {0.0, 0.0, 0.0};
-        this->p0[i] = -d/this->n[i];
-    }
-
-    Plane(const vec3_d &normal, const vec3_d &p0)
-    {
-        this->n = glm::normalize(normal);
-        double d = -glm::dot(p0, this->n);
-        this->normal_form = vec4_d(this->n, d);
-        this->p0 = p0;
-    }
+    // Plane(const vec4_d &normal_form)
+    // {
+    //     // init from normal form only
+    //     vec3_d norm = normal_form;
+    //     double nflen = glm::length(norm);
+    //     this->n = glm::normalize(norm);
+    //     double d = normal_form[3]/nflen;
+    //     this->normal_form = vec4_d(this->n, d);
+    //     int i = first_non_zero_index(normal_form);
+    //     this->p0 = {0.0, 0.0, 0.0};
+    //     this->p0[i] = -d/this->n[i];
+    // }
+    //
+    // Plane(const vec3_d &normal, const vec3_d &p0)
+    // {
+    //     this->n = glm::normalize(normal);
+    //     double d = -glm::dot(p0, this->n);
+    //     this->normal_form = vec4_d(this->n, d);
+    //     this->p0 = p0;
+    // }
 
     vec4_d get_normal_form() const
     {
@@ -190,9 +199,9 @@ class Plane
         return this->n;
     }
 
-    vec3_d get_p0() const
+    mat33_d get_pts() const
     {
-        return this->p0;
+        return this->pts;
     }
 
     bool point_in_plane(const vec3_d &p) const
@@ -251,8 +260,29 @@ class Sphere: public Hittable
     {
         return reflect_ray(dir, this->normal_at_surface(pos));
     }
-
 };
+
+
+// class Triangle: public Plane, public Hittable
+// {
+//     mat33_d pts;
+//
+//   public:
+//     Triangle(): Plane()
+//     {
+//         std::cout << glm::to_string(this->get_normal_form()) << std::endl;
+//     }
+//
+//     Triangle(const mat33_d &pts): Plane(pts)
+//     {
+//         this->pts = {this->get_p0(), pts[1], pts[2]}; // yes, this is strange.
+//     }
+//
+//     mat33_d get_pts() const
+//     {
+//         return this->pts;
+//     }
+// };
 
 
 // ------------------------ //
@@ -264,7 +294,7 @@ double line_plane_intersection(const Line &line, const Plane &plane)
     double l_dot_n = glm::dot(line.get_direction(), plane.get_n());
     if ( glm::epsilonEqual(l_dot_n, 0.0, PERCISION) )
         return inf;
-    double a = glm::dot(line.get_start()-plane.get_p0(), plane.get_n());
+    double a = glm::dot(line.get_start()-plane.get_pts()[0], plane.get_n());
     return -a/l_dot_n;
 }
 
@@ -275,8 +305,13 @@ double line_plane_intersection(const Line &line, const Plane &plane)
 int main()
 {
     // srand(time(NULL));
-    Sphere s;
-    std::cout << glm::to_string(s.reflect(X_, -X_+Y_)) << std::endl;
+    vec3_d p0 = {1.0, 0.0, 5.0};
+    vec3_d p1 = {0.0, 1.0, 5.0};
+    vec3_d p2 = {1.0, 1.0, 5.0};
+    mat33_d pts = {p0, p1, p2};
+    // Triangle t(pts);
+    Plane t;
+    std::cout << glm::to_string(t.get_n()) << std::endl;
 
     return 0;
 }
